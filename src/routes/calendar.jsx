b3461b56a93c67dbb4d7a2c85d1007f6a077e173
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-10-23 12:38:28 +0200

    :construction_worker: ADD | Missions & NavBar & Header Components

--- src/routes/calendar.jsx
+++ src/routes/calendar.jsx
@@ -1,28 +1,54 @@
 import Layout from "../layout";
 import MissionCard from "../components/missionCard";
+import Header from "../components/layout/Header";
+import NavBar from "../components/layout/NavBar";
 
-export default function Calendar() {
+const Missions = () => {
   const missions = [
     {
       points: "+100",
-      finish : true,
+      finish: true,
       id: 1,
       description: "Trouver quoi dire a Nicolas",
     },
     {
       points: "+100",
-      finish : false,
+      finish: false,
       id: 2,
-      description: "Trouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a Nicolas",
+      description:
+        "Trouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a Nicolas",
     },
   ];
+  return (
+    <section className="flex flex-col gap-4">
+      <h2 className="text-2xl font-medium">Défis</h2>
+
+      <ul className="flex flex-col gap-4">
+        {missions.map((mission) => (
+          <li key={mission.id} className="gap-2">
+            <MissionCard mission={mission} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default function Calendar() {
+  const meta = {
+    title: "Calendrier",
+    description: "Calendrier des défis",
+  };
 
   return (
     <Layout>
+      <Header title={meta.title} />
       <div className="p-6">
-        <h4>Calendar</h4>
-        <h1>L'asso du jour</h1>
-        <p>Relevez les défis que vous propose l’association du jour</p>
+        <div className="flex flex-col items-start gap-4 text-left">
+          <h1 className="text-4xl font-bold">L'asso du jour</h1>
+          <p className="text-2xl">Relevez les défis que vous propose l’association du jour</p>
+        </div>
+
         <div>
           <div>
             <img src="/"></img>
@@ -35,21 +61,9 @@ export default function Calendar() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <h2>Défis</h2>
-          <div>
-          <ul className="flex flex-col gap-4 mt-4">
-            {missions.map((mission) => (
-               
-                <li key={mission.id} className="gap-2">
-                <MissionCard mission={mission} />
-                </li>
-             
-            ))}
-            </ul>
-          </div>
-        </div>
+        <Missions />
       </div>
+      <NavBar title={meta.title} />
     </Layout>
   );
 }
