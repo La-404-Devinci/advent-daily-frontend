846875d3f75a274b7fbb92a47f9c0ac264ea76f5
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-10-26 17:53:15 +0200

    :sparkles: ADD | Added calendar page

--- src/routes/calendar.jsx
+++ src/routes/calendar.jsx
@@ -1,69 +1,106 @@
 import Layout from "../layout";
-import MissionCard from "../components/missionCard";
-import Header from "../components/layout/Header";
-import NavBar from "../components/layout/NavBar";
+import MissionCard from "../components/mission-card";
+import Header from "../components/layout/header";
+import Menu from "../components/layout/menu";
+import Logo from "../components/layout/logo";
+/*import {useEffect} from "react";*/
+
+const Asso = () => {
+
+    /*    useEffect(() => {
+            fetch(`PROCESS.ENV.DEV.API_URL${'/daily'}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }, []);*/
+
+
+    const data = {
+        name: "La 808 DeVinci",
+        description:
+            "Salut, ici la 808 devinci pour vous faire vibrer! Rejoignez nous au Pôle pour commencer vos défis",
+        avatar_url: "https://www.shutterstock.com/image-vector/wireframe-icon-thin-outline-style-260nw-1335621422.jpg",
+    };
+
+    return (
+        <div className="flex items-start gap-4 p-4 border border-blue-900 bg-gray-950 rounded-xl">
+            <Logo path={data.avatar_url} alt={data.alt}/>
+            <div className="flex flex-col gap-4">
+                <h2 className="text-2xl font-medium">{data.name}</h2>
+                <p className="text-white opacity-75">{data.description}</p>
+            </div>
+        </div>
+    );
+};
 
 const Missions = () => {
-  const missions = [
-    {
-      points: "+100",
-      finish: true,
-      id: 1,
-      description: "Trouver quoi dire a Nicolas",
-    },
-    {
-      points: "+100",
-      finish: false,
-      id: 2,
-      description:
-        "Trouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a NicolasTrouver quoi dire a Nicolas",
-    },
-  ];
-  return (
-    <section className="flex flex-col gap-4">
-      <h2 className="text-2xl font-medium">Défis</h2>
 
-      <ul className="flex flex-col gap-4">
-        {missions.map((mission) => (
-          <li key={mission.id} className="gap-2">
-            <MissionCard mission={mission} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+    /*   useEffect(() => {
+           fetch(`PROCESS.ENV.DEV.API_URL${'/daily/challenges'}`)
+               .then((response) => response.json())
+               .then((data) => {
+                   console.log(data);
+               })
+               .catch((error) => {
+                   console.error(error);
+               });
+       }, []);*/
+
+
+    const data = [
+        {
+            id: "1",
+            name: "Trouver quoi dire a Nicolas",
+            club_id: "1",
+            score: 100,
+            finish: true,
+        },
+        {
+            id: "2",
+            name: "Trouver quoi dire a Nicolas",
+            club_id: "1",
+            score: 100,
+            finish: false,
+        },
+    ];
+    return (
+        <section className="flex flex-col gap-4">
+            <h2 className="text-2xl font-medium">Défis</h2>
+            <ul className="flex flex-col gap-4">
+                {data.map((mission) => (
+                    <li key={mission.id} className="gap-2">
+                        <MissionCard mission={mission}/>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default function Calendar() {
-  const meta = {
-    title: "Calendrier",
-    description: "Calendrier des défis",
-  };
+    const meta = {
+        title: "Calendrier",
+        description: "Calendrier des défis",
+    };
 
-  return (
-    <Layout>
-      <Header title={meta.title} />
-      <div className="p-6">
-        <div className="flex flex-col items-start gap-4 text-left">
-          <h1 className="text-4xl font-bold">L'asso du jour</h1>
-          <p className="text-2xl">Relevez les défis que vous propose l’association du jour</p>
-        </div>
-
-        <div>
-          <div>
-            <img src="/"></img>
-          </div>
-          <div>
-            <h2>La 808 DeVinci</h2>
-            <p>
-              Salut, ici la 808 devinci pour vous faire vibrer! Rejoignez nous
-              au Pôle pour commencer vos défis
-            </p>
-          </div>
-        </div>
-        <Missions />
-      </div>
-      <NavBar title={meta.title} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Header title={meta.title}/>
+            <div className="flex flex-col gap-8 p-6 mt-16 mb-20 ">
+                <div className="flex flex-col items-start gap-4 text-left">
+                    <h1 className="text-4xl font-bold">L'asso du jour</h1>
+                    <p className="text-base">
+                        Relevez les défis que vous propose l’association du jour
+                    </p>
+                </div>
+                <Asso/>
+                <Missions/>
+            </div>
+            <Menu title={meta.title}/>
+        </Layout>
+    );
 }
