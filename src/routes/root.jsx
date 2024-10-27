e46ae0a8bc0fd9869ca42465b8a88e27becec6ce
Nicolas Becharat <78233186+miicolas@users.noreply.github.com>
2024-10-27 11:21:57 +0100

    Revert "Added Calendar Page"

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/buttons/Buttons";
+import { PrimaryButton } from "../components/buttons/Buttons";
 import Layout from "../layout";
 import { useForm } from "react-hook-form";
 
@@ -40,7 +40,7 @@ export default function Root() {
             </div>
 
             {errors.email && <p role="alert">{errors.email.message}</p>}
-            <Button styleType="primary" type="Buttont">Continue</Button>
+            <PrimaryButton type={"submit"}>Continue</PrimaryButton>
           </form>
           <p>
             J'ai déjà un 404ID{" "}
