ec2f1795ff38fb5a5e5ddc6acd017d297a81a429
Michel Moccand-J. <michelm.gaming@gmail.com>
2024-10-28 14:36:42 +0100

    ci: testing pr

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -24,7 +24,7 @@ export default function Root() {
     <Layout>
       <div className="flex items-center justify-center min-h-screen">
         <div className="text-center">
-          <h1 className="text-4xl font-bold">Bienvenue !</h1>
+          <h1 className="text-4xl font-bold">Test !</h1>
           <p className="mt-4 text-3xl">Relève des défis</p>
           <form
             onSubmit={handleSubmit(onSubmit)}
