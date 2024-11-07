8b05281f2b87cb895e508786d508a0dc359c18d8
Paul Boisaubert <146094035+LeCodeurDeParis@users.noreply.github.com>
2024-11-07 21:59:00 +0100

    fix: edit user width for responsive

--- src/components/edit-user-info.jsx
+++ src/components/edit-user-info.jsx
@@ -1,29 +1,29 @@
 export default function EditUserInfo({ user }) {
     return (
-        <div className="flex flex-col items-start gap-6">
-            <div className="flex flex-col items-start gap-1.5 opacity-50">
+        <div className="flex flex-col items-start gap-6 px-2">
+            <div className="flex flex-col items-start gap-1.5 opacity-50 w-full">
                 <p>Email</p>
                 <input
                     type="email"
-                    className="w-96 p-2 border border-gray-300 rounded-md"
+                    className="w-full p-2 border border-gray-300 rounded-md"
                     value={user.email}
                     disabled
                 />
                 <p className="text-[#64748B]">L'email ne peut pas être modifié après la création du compte.</p>
             </div>
-            <div className="flex flex-col items-start gap-1.5">
+            <div className="flex flex-col items-start gap-1.5 w-full">
                 <p>Nom d'utilisateur</p>
                 <input
                     type="text"
-                    className="w-96 p-2 border border-gray-300 rounded-md text-black"
+                    className="w-full p-2 border border-gray-300 rounded-md text-black"
                     value={user.username}
                 />
             </div>
-            <div className="flex flex-col items-start gap-1.5">
+            <div className="flex flex-col items-start gap-1.5 w-full">
                 <p>Citation</p>
                 <input
                     type="text"
-                    className="w-96 p-2 border border-gray-300 rounded-md text-black"
+                    className="w-full p-2 border border-gray-300 rounded-md text-black"
                     value={user.quote}
                 />
             </div>
