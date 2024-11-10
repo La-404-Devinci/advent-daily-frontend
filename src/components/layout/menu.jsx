5ff66fb6726d33cba123cd8b61afcfc1206c05c9
Matteo <matteo.lorenzoq@gmail.com>
2024-11-10 22:44:36 +0100

    chore: upgrade menu ui

--- src/components/layout/menu.jsx
+++ src/components/layout/menu.jsx
@@ -1,4 +1,4 @@
-import { Home, Star, UserRound } from "lucide-react";
+import { CalendarDays, ChartNoAxesColumn, UserRound } from "lucide-react";
 import { useLocation } from "react-router-dom";
 import { cn } from "../../libs/functions";
 
@@ -6,7 +6,7 @@ const Items_NavBar = [
   {
     title: "Calendrier",
     path: "/calendar",
-    icon: <Home className="w-6 h-6" />,
+    icon: <CalendarDays className="w-6 h-6" />,
   },
   {
     title: "Profil",
@@ -16,7 +16,7 @@ const Items_NavBar = [
   {
     title: "Classement",
     path: "/leaderboard",
-    icon: <Star className="w-6 h-6" />,
+    icon: <ChartNoAxesColumn className="w-6 h-6" />,
   },
 ];
 
@@ -25,17 +25,19 @@ export default function Menu() {
   const pathname = useLocation().pathname;  
   
   return (
-    <nav className="fixed bottom-0 left-0 z-50 flex items-center justify-between w-full h-20 px-4 m-auto text-gray-300 bg-black sm:left-1/2 sm:-translate-x-1/2">
-      <ul className="flex items-center justify-between max-w-[30rem] gap-2 mx-auto">
+    <nav className="fixed bottom-0 left-0 z-50 flex items-center justify-between w-full 
+      h-20 px-4 m-auto text-gray-300 bg-gray-950 sm:left-1/2 sm:-translate-x-1/2"
+    >
+      <ul className="flex items-center justify-between gap-1 w-full max-w-[30rem] mx-auto">
         {Items_NavBar.map((item) => (
-          <li key={`menu:${item.title}`} className="w-28 h-full">
+          <li key={`menu:${item.title}`} className="flex-1 h-full">
             <a
               href={item.path}
               className={cn(
-                "flex flex-col items-center gap-2 p-3 text-xl rounded-md",
+                "flex flex-col items-center gap-2 py-3 text-xl",
                 pathname.includes(item.path)
-                  ? "text-blue-800"
-                  : "hover:bg-blue-950 hover:bg-opacity-40"
+                  ? "text-gray-50 bg-blue-800/25 font-semibold"
+                  : "hover:bg-blue-950/25"
               )}
             >
               {item.icon}
