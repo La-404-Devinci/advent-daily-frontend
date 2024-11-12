5a8c99f8318ab40a6e3980b25ccd1b9dbd0c9079
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-12 16:42:11 +0100

    chore: submit new club

--- src/components/modal-asso.jsx
+++ src/components/modal-asso.jsx
@@ -1,27 +1,54 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "../components/buttons/Buttons.jsx";
+import React, {useState, useEffect} from "react";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Button} from "../components/buttons/Buttons.jsx";
 import Input from "../components/ui/input.jsx";
+import { useNavigate } from 'react-router'
+
 
 const schema = z.object({
-    name: z.string().min(1, { message: "Le nom de l'asso est obligatoire" }),
-
+    name: z.string().min(1, {message: "Le nom de l'asso est obligatoire"}),
+    avatarUrl: z.string().url({message: "L'avatar doit être une URL"}),
 });
 
-const ModalAsso = ({ isOpen, onClose }) => {
+const ModalAsso = ({isOpen, onClose}) => {
+
+
+    const navigate = useNavigate()
 
     const [name, setName] = useState("");
+    const [avatarUrl, setAvatarUrl] = useState("");
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const {register, handleSubmit, formState: {errors}} = useForm({
         resolver: zodResolver(schema),
 
     })
 
-    const onSubmit = (data) => {
-            console.log(data, "create");
-            onClose();
+    const onSubmit = async (data) => {
+
+            await fetch(`${import.meta.env.VITE_API_URL}/admin/clubs`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-ADMIN-KEY': import.meta.env.VITE_ADMIN_KEY
+                },
+                body: JSON.stringify({
+                    name: data.name,
+                    avatarUrl: data.avatarUrl,
+                    dailyDate: new Date().toISOString().split('T')[0],
+                    description: "Description de l'asso"
+                })
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);})
+                .catch((error) => {
+                    console.error(error);
+                });
+
+        onClose();
+        navigate(0)
     };
 
     if (!isOpen) return null;
@@ -30,7 +57,7 @@ const ModalAsso = ({ isOpen, onClose }) => {
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50 bg-gray-950">
             <div className="flex flex-col justify-between mx-10 bg-black border border-blue-700 rounded-xl h-fit w-96">
                 <h2 className="p-4 text-lg font-bold">Ajouter une asso</h2>
-                <hr className="border-blue-700" />
+                <hr className="border-blue-700"/>
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 p-4">
                     <Input
                         errors={errors}
@@ -43,11 +70,23 @@ const ModalAsso = ({ isOpen, onClose }) => {
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                     />
+                    <Input
+                        errors={errors}
+                        register={register}
+                        name="avatarUrl"
+                        type="text"
+                        label="Avatar"
+                        id="avatarUrl"
+                        placeholder="Lien de l'avatar"
+                        value={avatarUrl}
+                        onChange={(e) => setAvatarUrl(e.target.value)}
+                    />
                     <div className="flex justify-end gap-2">
-                        <Button styleType="secondary" type="button" onClick={onClose}>
+                        <Button styleType="secondary" type="button" onClick={onClose}
+                                className={"px-4 py-2 w-fit h-fit"}>
                             Annuler
                         </Button>
-                        <Button styleType="primary" type="submit">
+                        <Button styleType="primary" type="submit" className={"px-4 py-2 w-fit h-fit"}>
                             Ajouter
                         </Button>
                     </div>
