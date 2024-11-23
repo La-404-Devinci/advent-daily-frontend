2cb6e4a8abb95f14fd1862e95ffd2795a4433202
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-23 11:53:44 +0100

    feat: login on confirmation page

--- src/routes/confirmation-email.jsx
+++ src/routes/confirmation-email.jsx
@@ -11,13 +11,21 @@ export default function ConfirmationEmail() {
                     <h1 className="text-5xl font-bold">Un mail t'as été envoyé</h1>
                     <p className="mt-4 text-2xl text-white-300">Regarde tes spams</p>
                 </div>
-                <form className="flex flex-col justify-center items-center w-full gap-8 py-24">
+                <form className="flex flex-col justify-center items-center w-full gap-8 py-20">
                     <MailOpen className="h-24 w-24 "/>
                     <div className=" flex flex-col items-start w-full">
                         <input type="text" placeholder={email} value={email} disabled
                                className=" w-full py-2 pl-3 pr-8 mt-2 bg-white border border-gray-300 rounded-md focus:border-blue-900 text-gray-950"/>
                     </div>
                 </form>
+                <p className="flex flex-col text-center">
+                    J'ai pas un 404ID{" "}
+                    <span>
+                        <a href="/login" className="font-medium text-blue-400 underline">
+                            Me connecter
+                        </a>
+                    </span>
+                </p>
             </div>
         </Layout>
     );
